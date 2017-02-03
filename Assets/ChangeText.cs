96c1a2a8a383bff432765d16f2551bd196e8c998
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-03 14:28:14 +0000

    Added script that can change dialogue

--- Assets/ChangeText.cs
+++ Assets/ChangeText.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class ChangeText : MonoBehaviour {
+public class ChangeText : MonoBehaviour
+{
+    Text m_Dialogue;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        m_Dialogue = gameObject.GetComponent<Text>();
+        m_Dialogue.text = "<insert other dialogue";
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
 }
