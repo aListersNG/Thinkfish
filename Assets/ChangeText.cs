db525d8c35cfa9ce5ba2928f74c3c7ccf6cdb464
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-03 14:46:55 +0000

    Dialogue text now changes on pressing E

--- Assets/ChangeText.cs
+++ Assets/ChangeText.cs
@@ -5,18 +5,40 @@ using System.Collections;
 public class ChangeText : MonoBehaviour
 {
     Text m_Dialogue;
-
+    int i = 0;
 	// Use this for initialization
 	void Start ()
     {
-        m_Dialogue = gameObject.GetComponent<Text>();
-        m_Dialogue.text = "<insert other dialogue";
-
+        
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-	    
-	}
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            m_Dialogue = gameObject.GetComponent<Text>();
+            if (i == 0)
+            {
+                m_Dialogue.text = "<insert other dialogue>";
+            }
+
+            if (i == 1)
+            {
+                m_Dialogue.text = "<insert other other dialogue>";
+            }
+
+            if (i == 2)
+            {
+                m_Dialogue.text = "<insert reating dialogue>";
+            }
+
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            i++;
+        }
+    }
 }
