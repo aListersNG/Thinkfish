1e204817f3c333516f9c147303fdafcc35518fb3
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-10 15:29:46 +0000

    added more rooms to the into work interior

---
+++ Assets/Scripts/Scene Into Work Inside/Load_IntoWork_entrance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_IntoWork_entrance : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)// Developed by Andrew McGonigal
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("Alpha_Into_Work_inside");//modified by David Law
+
+        }
+    }
+}
