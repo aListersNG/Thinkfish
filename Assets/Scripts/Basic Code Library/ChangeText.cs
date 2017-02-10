a5e7be4703a5d750ec883b55d4ea14d14e1bccc3
unknown <1401045@MC10634.uad.ac.uk>
2017-02-10 15:45:44 +0000

    Small update to make text changing re-useable for designers

--- Assets/Scripts/Basic Code Library/ChangeText.cs
+++ Assets/Scripts/Basic Code Library/ChangeText.cs
@@ -4,12 +4,13 @@ using System.Collections;
 
 public class ChangeText : MonoBehaviour
 {
-    Text m_Dialogue;
+    public Text m_Dialogue;
+	public string[] m_dialogueOption;
     int i = 0;
 	// Use this for initialization
 	void Start ()
     {
-        
+		m_Dialogue.text = m_dialogueOption [i];
     }
 
     // Update is called once per frame
@@ -18,27 +19,31 @@ public class ChangeText : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
-            m_Dialogue = gameObject.GetComponent<Text>();
-            if (i == 0)
-            {
-                m_Dialogue.text = "<insert other dialogue>";
-            }
-
-            if (i == 1)
-            {
-                m_Dialogue.text = "<insert other other dialogue>";
-            }
-
-            if (i == 2)
-            {
-                m_Dialogue.text = "<insert reating dialogue>";
-            }
+			m_Dialogue.text = m_dialogueOption [i];
+            //m_Dialogue = gameObject.GetComponent<Text>();
+            //if (i == 0)
+            //{
+            //    m_Dialogue.text = "<insert other dialogue>";
+            //}
+			//
+            //if (i == 1)
+            //{
+            //    m_Dialogue.text = "<insert other other dialogue>";
+            //}
+			//
+            //if (i == 2)
+            //{
+            //    m_Dialogue.text = "<insert reating dialogue>";
+            //}
 
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            i++;
+			if (i < 2) {
+				i++;
+			} else {
+				i = 0;
+			}
         }
     }
 }
