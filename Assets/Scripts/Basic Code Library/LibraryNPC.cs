a836ad9ad5031722971ff1ff4811313f852c9cf1
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2017-02-11 20:27:53 +0000

    Npc in library complete with current timer place holder

--- Assets/Scripts/Basic Code Library/LibraryNPC.cs
+++ Assets/Scripts/Basic Code Library/LibraryNPC.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LibraryNPC : MonoBehaviour {
 
+    enum GameType { CheckInBook, PrinterUsage, CheckInComputer};
+
     //Used for deciding the movement
     bool walkingIn, walkingOut, idle;
+
+    public String[] bookCheckIn;
+    public String[] printerUsage;
+    public String[] computerCheckIn;
 
     public bool gamePlaying;
 
     Rigidbody2D mybody;
 
+    GameType questType;
+
+    float tempGameTimer = 0.0f;
+
 	// Use this for initialization
 	void Start () {
         walkingIn = true;
         mybody = GetComponent<Rigidbody2D>();
-	}
+        RandomGameType();
+        SetText();
+        }
 	
 	// Update is called once per frame
 	void Update () {
@@ -43,6 +56,9 @@ public class LibraryNPC : MonoBehaviour {
             walkingIn = false;
             idle = true;
             mybody.velocity = new Vector2(0.0f, 0.0f);
+            gamePlaying = true;
+            GetComponent<ChangeText>().enabled = true;
+            GetComponent<ChangeText>().m_Dialogue.text = GetComponent<ChangeText>().m_dialogueOption[0];
         }
         else
         {
@@ -65,11 +81,70 @@ public class LibraryNPC : MonoBehaviour {
 
     void ResetNPC()
     {
+        walkingOut = false;
+        walkingIn = true;
         transform.position = new Vector3(10.0f, 0.0f, transform.position.z);
+        RandomGameType();
+        SetText();
     }
 
     void GamePlay()
     {
+        if(!gamePlaying)
+        {
+            walkingOut = true;
+            idle = false;
+            GetComponent<ChangeText>().m_Dialogue.text = "";
+            GetComponent<ChangeText>().enabled = false;
+        }
+        else
+        {
+            //Display any appropriate text
+            //Run the right game
+            if(tempGameTimer >= 2.0f)
+            {
+                tempGameTimer = 0.0f;
+                gamePlaying = false;
+            }
+            else
+            {
+                tempGameTimer += Time.deltaTime;
+            }
+        }
+    }
 
+    void RandomGameType()
+    {
+        questType = (GameType)UnityEngine.Random.Range(0, Enum.GetNames(typeof(GameType)).Length);
+    }
+
+    void SetText()
+    {
+        ChangeText text = GetComponent<ChangeText>();
+        
+        switch(questType)
+        {
+            case GameType.CheckInBook:
+                for (int i = 0; i <= 4; i++)
+                {
+                    text.m_dialogueOption[i] = bookCheckIn[i];
+                }
+
+                break;
+            case GameType.CheckInComputer:
+                for (int i = 0; i <= 4; i++)
+                {
+                    text.m_dialogueOption[i] = computerCheckIn[i];
+                }
+
+                break;
+            case GameType.PrinterUsage:
+                for (int i = 0; i <= 4; i++)
+                {
+                    text.m_dialogueOption[i] = printerUsage[i];
+                }
+
+                break; 
+        }
     }
 }
