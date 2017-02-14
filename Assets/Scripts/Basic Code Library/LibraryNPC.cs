c4840ba941c66bc31d7a2b74f74c040410528c72
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-14 12:13:55 +0000

    into work building and dialogue pretty much finished

--- Assets/Scripts/Basic Code Library/LibraryNPC.cs
+++ Assets/Scripts/Basic Code Library/LibraryNPC.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class LibraryNPC : MonoBehaviour {
 
+    //The different game types that can be used
     enum GameType { CheckInBook, PrinterUsage, CheckInComputer};
 
     //Used for deciding the movement
     bool walkingIn, walkingOut, idle;
 
+    //To display appropriate texts
     public String[] bookCheckIn;
     public String[] printerUsage;
     public String[] computerCheckIn;
+	public Text myText;     //<-- To be implemented for use
 
+    //If the npc's "game" is being played
     public bool gamePlaying;
 
+    //For the body movement
     Rigidbody2D mybody;
 
+    //To hold the game type
     GameType questType;
 
+    //Current placeholder to make the character stop
+    //Mostly for testing that the walkout worked too
     float tempGameTimer = 0.0f;
 
 	// Use this for initialization
 	void Start () {
+        //Set all the default variables
         walkingIn = true;
         mybody = GetComponent<Rigidbody2D>();
         RandomGameType();
@@ -33,6 +43,7 @@ public class LibraryNPC : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        //Run the appropriate section
 		if(walkingIn)
         {
             WalkingIn();
@@ -50,6 +61,7 @@ public class LibraryNPC : MonoBehaviour {
 
     void WalkingIn()
     {
+        //If the character is in position already
         if(transform.position.x <= 0.0f)
         {
             //stop the character from moving
@@ -69,18 +81,22 @@ public class LibraryNPC : MonoBehaviour {
 
     void WalkingOut()
     {
+        //If the npc is off the screen
+        //Reset all it's variables
         if(transform.position.x <= -12.0f)
         {
             ResetNPC();
         }
         else
         {
+            //Move it off the screen
             mybody.velocity = new Vector2(-2.0f, 0.0f);
         }
     }
 
     void ResetNPC()
     {
+        //Set to new starting variables
         walkingOut = false;
         walkingIn = true;
         transform.position = new Vector3(10.0f, 0.0f, transform.position.z);
@@ -90,6 +106,7 @@ public class LibraryNPC : MonoBehaviour {
 
     void GamePlay()
     {
+        //If the game is no longer being played, allow the character to leave
         if(!gamePlaying)
         {
             walkingOut = true;
@@ -115,13 +132,16 @@ public class LibraryNPC : MonoBehaviour {
 
     void RandomGameType()
     {
+        //Choose a random game type to begin
         questType = (GameType)UnityEngine.Random.Range(0, Enum.GetNames(typeof(GameType)).Length);
     }
 
     void SetText()
     {
+        //Currently a placeholder using the ChangeText script
         ChangeText text = GetComponent<ChangeText>();
         
+        //Dependant on what game type it is, display appropriate text
         switch(questType)
         {
             case GameType.CheckInBook:
