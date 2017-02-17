0f7f7cbf1c1f9a77035172bf251d59b05a70c1d6
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-17 19:03:15 +0000

    added all the game except the scoring because I got stuck

---
+++ Assets/Scripts/Scene Library Bookshelves/PickupGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupGame : MonoBehaviour
+{
+    public int score;
+    public int howManyBooks;
+    public GameObject prefab;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(prefab, new Vector3(UnityEngine.Random.Range(0f, 5f), -2.5f, 0), Quaternion.identity);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void increaseScore()
+    {
+        score++;
+    }
+
+    
+}
