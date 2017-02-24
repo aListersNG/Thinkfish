c819b043833c70e8f250cb598f3069b6910a1049
unknown <1401045@MC10651.uad.ac.uk>
2017-02-24 17:08:18 +0000

    Getting the leaving bakery to actually sync up

--- Assets/Scripts/Basic Code Library/Load_New_Scene.cs
+++ Assets/Scripts/Basic Code Library/Load_New_Scene.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Load_New_Scene : MonoBehaviour {
 
     public string m_Scene;
+    public bool inTrigger;
 
-    void OnTriggerEnter2D(Collider2D col)// Developed by Andrew McGonigal
+    void OnTriggerStay2D(Collider2D col)// Developed by Andrew McGonigal
     {
-        if (col.gameObject.tag == "Player")
+        
+        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
         {
+            inTrigger = true;
             Application.LoadLevel(m_Scene);//modified by David Law
 
         }
