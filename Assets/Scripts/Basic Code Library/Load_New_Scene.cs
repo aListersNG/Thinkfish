19a74348ada0b5aa429f28d36982d277deb01de2
alistersNG <davidofteampower@hotmail.co.uk>
2017-02-27 11:56:45 +0000

    Made some minor changes and bug fixes

--- Assets/Scripts/Basic Code Library/Load_New_Scene.cs
+++ Assets/Scripts/Basic Code Library/Load_New_Scene.cs
@@ -10,7 +10,7 @@ public class Load_New_Scene : MonoBehaviour {
     void OnTriggerStay2D(Collider2D col)// Developed by Andrew McGonigal
     {
         
-        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        if (col.gameObject.tag == "Player")
         {
             inTrigger = true;
             Application.LoadLevel(m_Scene);//modified by David Law
