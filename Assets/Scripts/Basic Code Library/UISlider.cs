13e1218c95b0e76f06168f462e3a0353bf8c02a4
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2017-03-18 19:37:43 +0000

    Time slider now active in scene

---
+++ Assets/Scripts/Basic Code Library/UISlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISlider : MonoBehaviour {
+
+    float startX, EndX;
+
+	// Use this for initialization
+	void Start () {
+        startX = transform.position.x;
+        EndX = startX + 510.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timeKeeper = Time.deltaTime;
+
+        if (EndX >= transform.position.x + timeKeeper)
+        {
+            transform.position = new Vector3(transform.position.x + timeKeeper, transform.position.y, transform.position.z);
+        }
+	}
+}
