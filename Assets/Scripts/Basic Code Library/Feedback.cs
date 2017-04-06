f832a4830cd4bd4ba34494b954f0a4fb25104bcb
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2017-04-06 17:38:59 +0100

    Little update for feedback system

--- Assets/Scripts/Basic Code Library/Feedback.cs
+++ Assets/Scripts/Basic Code Library/Feedback.cs
@@ -6,6 +6,26 @@ public class Feedback : MonoBehaviour {
 
     int overallScore = 0, npcTalkedto = 0, trashPickedUp = 0;
     float timeTakenChallenges = 0.0f, timeDistracted = 0.0f;
+    public bool dayFinished;
+
+    void Update()
+    {
+        if(dayFinished)
+        {
+
+            //Do whatever needed before this point
+            ResetVariables();
+        }
+    }
+
+    void ResetVariables()
+    {
+        overallScore = 0;
+        npcTalkedto = 0;
+        trashPickedUp = 0;
+        timeTakenChallenges = 0.0f;
+        timeDistracted = 0.0f;
+    }
 
     public void AddTalkedTo()
     {
@@ -19,6 +39,26 @@ public class Feedback : MonoBehaviour {
 
     public void SetOverallScore(int score)
     {
+        if(score>25)
+        {
+            score = 25;
+        }
+        else if (score<0)
+        {
+            score = 0;
+        }
+
         overallScore += score;
     }
+
+    public void AddTimeDistracted(float time)
+    {
+        timeDistracted += time;
+    }
+
+    public void AddChallengeTime(float time)
+    {
+        timeTakenChallenges += time;
+    }
+
 }
