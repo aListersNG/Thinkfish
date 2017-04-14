b39ae3ae737e1429e90175217562baf75010d399
unknown <1401045@MC10649.uad.ac.uk>
2017-04-14 13:12:23 +0100

    Timer now only plays when the character goes in the library for the first time

--- Assets/Scripts/Basic Code Library/UISlider.cs
+++ Assets/Scripts/Basic Code Library/UISlider.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class UISlider : MonoBehaviour {
 
     float startX, EndX;
-    bool gameOneComplete, gameTwoComplete, active;
+    bool gameOneComplete, gameTwoComplete;
+    public Camera myCamera;
+    public bool active;
 
 	// Use this for initialization
 	void Start () {
         startX = transform.position.x;
         EndX = startX + 510.0f;
+        active = false;
 
         Feedback feed = GameObject.FindGameObjectWithTag("FeedbackSystem").GetComponent<Feedback>();
         if (feed)
         {
             if(feed.GetTasksCompleted() == 1)
             {
+                active = true;
                 transform.position = new Vector3((510 / 3) + startX, transform.position.y, transform.position.z);
                 gameOneComplete = true;
             }
             else if(feed.GetTasksCompleted() == 2)
             {
+                active = true;
                 transform.position = new Vector3((510 / 3)*2 + startX, transform.position.y, transform.position.z);
                 gameOneComplete = true;
                 gameTwoComplete = true;
@@ -59,5 +64,13 @@ public class UISlider : MonoBehaviour {
                 GameObject.FindGameObjectWithTag("FeedbackSystem").GetComponent<Feedback>().dayFinished = true;
             }
         }
+        else
+        {
+            if(myCamera.enabled)
+            {
+                active = true;
+            }
+        }
+
 	}
 }
